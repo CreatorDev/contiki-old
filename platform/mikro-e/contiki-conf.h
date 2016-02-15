2a1ae7bb185a434612de904ff8b63bd071bb5097
Pratik Prajapati <Pratik.Prajapati@imgtec.com>
2016-02-15 13:48:56 +0530

    platform/mikro-e: define global IP in contiki-conf
    Change-Id: Ia4ef2efd81cab67e0b3b1e3fe5c02623421fce7e
    Signed-off-by: Pratik Prajapati <Pratik.Prajapati@imgtec.com>

--- platform/mikro-e/contiki-conf.h
+++ platform/mikro-e/contiki-conf.h
@@ -35,12 +35,18 @@
 #include <inttypes.h>
 #include "cc2520-conf.h"
 
+/* Include project config file if defined in the project Makefile */
+#ifdef PROJECT_CONF_H
+  #include "project-conf.h"
+#endif /* PROJECT_CONF_H */
+
 #define CCIF
 #define CLIF
 
 typedef uint16_t uip_stats_t;
 typedef uint32_t clock_time_t;
 typedef uint32_t rtimer_clock_t;
+
 #define RTIMER_CLOCK_LT(a,b) ((int32_t)((a)-(b)) < 0)
 
 #define CLOCK_CONF_SECOND                       1024
@@ -48,17 +54,24 @@ typedef uint32_t rtimer_clock_t;
 #define NETSTACK_CONF_WITH_IPV6                 1
 #define NETSTACK_CONF_RADIO                     cc2520_driver
 #define NETSTACK_CONF_FRAMER                    framer_802154
-#define NETSTACK_CONF_RDC                       nullrdc_driver
+#ifndef NETSTACK_CONF_RDC
+  #define NETSTACK_CONF_RDC                     nullrdc_driver
+#endif
 #define NETSTACK_CONF_MAC                       nullmac_driver
 #define NETSTACK_CONF_NETWORK                   sicslowpan_driver
-#define RF_CHANNEL                              26
-#define IEEE802154_PANID                        0xABCD
-//#define FIXED_MAC_ADDRESS                       0xDEADBEEFDEADBEEF
+#ifndef RF_CHANNEL
+  #define RF_CHANNEL                            26
+#endif
+#ifndef IEEE802154_PANID
+  #define IEEE802154_PANID                      0xABCD
+#endif
+//#define FIXED_MAC_ADDRESS                     0xDEADBEEFDEADBEEF
 
 #define UIP_CONF_ROUTER                         0
 #define LINKADDR_CONF_SIZE                      8
-#define UIP_CONF_BUFFER_SIZE                    1024
-
+#ifndef UIP_CONF_BUFFER_SIZE
+  #define UIP_CONF_BUFFER_SIZE                  1024
+#endif
 
 /* UDP configuration options */
 #define UIP_CONF_UDP                            1
@@ -88,9 +101,15 @@ typedef uint32_t rtimer_clock_t;
 #define LED1                                    LEDS_RED
 #define LED2                                    LEDS_GREEN
 
-/* Include project config file if defined in the project Makefile */
-#ifdef PROJECT_CONF_H
-  #include "project-conf.h"
-#endif /* PROJECT_CONF_H */
+#ifndef GLOBAL_IPv6_ADDR
+  #define GLOBAL_IPv6_ADDR1                     0x2001
+  #define GLOBAL_IPv6_ADDR2                     0x1418
+  #define GLOBAL_IPv6_ADDR3                     0x0100
+  #define GLOBAL_IPv6_ADDR4                     0x823c
+  #define GLOBAL_IPv6_ADDR5                     0x0
+  #define GLOBAL_IPv6_ADDR6                     0x0
+  #define GLOBAL_IPv6_ADDR7                     0x0
+  #define GLOBAL_IPv6_ADDR8                     0x0
+#endif
 
 #endif /* CONTIKI_CONF_H */
--- platform/mikro-e/net-init.c
+++ platform/mikro-e/net-init.c
@@ -1,32 +1,30 @@
-/*
- * Copyright (c) 2015, Imagination Technologies Limited and/or its
- * affiliated group companies.
+/**
+ * Copyright (c) 2016, Imagination Technologies Limited
  *
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote
- *    products derived from this software without specific prior
- *    written permission.
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ *    and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ *    endorse or promote products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <contiki.h>
@@ -87,7 +85,8 @@ net_init()
   process_start(&tcpip_process, NULL);
 
   lladdr = uip_ds6_get_link_local(-1);
-  uip_ip6addr(&ipaddr, 0x2001, 0x1418, 0x100, 0x823c, 0, 0, 0, 0);
+  uip_ip6addr(&ipaddr, GLOBAL_IPv6_ADDR1, GLOBAL_IPv6_ADDR2, GLOBAL_IPv6_ADDR3, GLOBAL_IPv6_ADDR4,
+    GLOBAL_IPv6_ADDR5, GLOBAL_IPv6_ADDR6, GLOBAL_IPv6_ADDR7, GLOBAL_IPv6_ADDR8);
   uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
   uip_ds6_addr_add(&ipaddr, 0, ADDR_TENTATIVE);
 
