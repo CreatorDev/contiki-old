27f70c132175ebab72294cccac8fe2afee8657ed
Pratik Prajapati <Pratik.Prajapati@imgtec.com>
2016-03-12 17:44:47 +0530

    platform/mikro-e/dev: add relay-click driver
    Also add relay-test application in platform/mikro-e/apps.
    
    Signed-off-by: Pratik Prajapati <Pratik.Prajapati@imgtec.com>

---
+++ platform/mikro-e/dev/relay-click.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016, Imagination Technologies Limited and/or its affiliated
+ * group companies.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * \file
+ *         Relay click header file.
+ *         http://www.mikroe.com/click/relay/
+ */
+
+#ifndef RELAY_CLICK_H_
+#define RELAY_CLICK_H_
+
+#define RELAY1    1
+#define RELAY2    2
+
+void relay_click_init();
+void relay_on(int8_t relay_num);
+void relay_off(int8_t relay_num);
+void relay_toggle(int8_t relay_num);
+
+#endif /* RELAY_CLICK_H_ */
